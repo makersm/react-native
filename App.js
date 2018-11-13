02d52413f39e4a6ac60854376a574d204d691527
이소민 <sayyo1120@enjoyworks.co.kr>
2018-11-13 18:01:19 +0900

    first

---
+++ App.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {Platform, StyleSheet, Text, View, Image, AppRegistry} from 'react-native';
+var MOCKED_MOVIES_DATA = [
+    {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    var movie = MOCKED_MOVIES_DATA[0];
+    return (
+        <View style={styles.container}>
+          <Text>{movie.title}</Text>
+          <Text>{movie.year}</Text>
+          <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  thumbnail: {
+      width: 53,
+      height: 81,
+  },
+});
